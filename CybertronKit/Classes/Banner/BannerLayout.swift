0ad184481f30d0e0346151f619a06c345a0a44d0
wayne <wayne_sun@jiliguala.com>
2022-05-05 21:55:30 +0800

    Add BannerLayout code outline.

--- CybertronKit/Classes/Banner/BannerLayout.swift
+++ CybertronKit/Classes/Banner/BannerLayout.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 class BannerLayout: UICollectionViewLayout {
+    private var itemCount: Int = 0
+    private var itemSize: CGSize = .zero
     
+    
+    override func prepare() {
+        guard let collection = self.collectionView else { return }
+        
+        itemCount = collection.numberOfItems(inSection: 0)
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return self.collectionView?.frame.size ?? .zero
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return false
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layouts = super.layoutAttributesForElements(in: rect)
+        layouts?.forEach({ att in
+            att.zIndex
+        })
+        return nil
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return nil
+    }
 }
